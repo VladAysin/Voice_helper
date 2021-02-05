4b22a7574411b78d33af16b0a90581473d5e8b0d
krabik1611 <danilmelnikov11@gmail.com>
2021-02-05 22:13:48 +0500

    add some scripts

--- Backend/commands/scripts/script.py
+++ Backend/commands/scripts/script.py
@@ -16,4 +16,25 @@ def weather():
     r = get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={key}&lang=ru").json()
     answer = r["main"]
     answer.update(r["weather"][0])
-    print("requests\n",answer)
+    print(answer)
+
+def launchProgramm(program:str):
+    programs = {
+        "explorer":["проводник","мой компьютер","explorer"],
+        "browser":["браузер","сайт росатома"]
+    }
+    try:
+        if program in programs["explorer"] :
+            program = "explorer"
+        elif program in programs["browser"] :
+            program = "https://rosatom.ru/"
+        os.system(f"start {program}")
+    except Exception as err:
+        print("Error",err)
+    return None
+
+            
+
+
+if __name__ == "__main__":
+    launchProgramm("браузер")
