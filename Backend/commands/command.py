23c1b87e827527ca252b03c9745e0abdce9dee83
gutjei <ya-ruslan290@yandex.ru>
2021-02-07 08:12:07 +0500

    Night Ver

--- Backend/commands/command.py
+++ Backend/commands/command.py
@@ -17,6 +17,7 @@ def run_command(command, config):
         return lemma_command
 
     def find_command(our_command,commands):
+        copy_command = our_command.copy()
         while True:
             
             if type(commands) == types.FunctionType:
@@ -29,21 +30,33 @@ def run_command(command, config):
                     if word in our_command:
                         commands = commands[words[lemma_words.index(word)]]
                         our_command.remove(word)
-
+                if len(our_command) == len(copy_command):
+                    break
         return commands
-
     global commands
+
+    pdffind = ['в', 'документе']
+    for word in lemmatizing(pdffind):
+        some_command = lemmatizing(command.split(' '))
+        if word in some_command:
+            return findInPDF(" ".join(command.split(" ")[2:]),'./')
+        # cum = ' '.join(command)
+        # com = cum.replace(' '.join(pdffind),'')
+        
+
     command = lemmatizing(command.split(" "))
     analysis = ["оборудование","состояние"]
     for word in lemmatizing(analysis):
         if word in command:
             return xls_analysis(command)
+
     file_found = ['файл']
     for word in lemmatizing(file_found):
         if word in command:
             return find_file_on_fs('flag.txt')
-    command = find_command(command,commands)
+
     try:
+        command = find_command(command,commands)
         return command() 
     except:
         return {"type": "text", "data": "Команда не может быть выполнена"}
