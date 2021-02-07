4230825c88e1fb073820879340352ed3848216d2
krabik1611 <danilmelnikov11@gmail.com>
2021-02-07 08:45:16 +0500

    change gui

---
+++ GUI/main.py
@@ -0,0 +1,52 @@
+from kivy.app import App
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+import os
+from kivy.config import Config
+from kivy.properties import ObjectProperty
+from kivymd.uix.label import MDLabel
+
+
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '600')
+Config.set('graphics', 'resizable', False)
+Config.write()
+
+
+Builder.load_file(os.path.join(os.path.dirname(os.path.abspath(__file__)),"style.kv"))
+                                
+                                    
+
+class Screen(BoxLayout):
+    def __init__(self):
+
+        super(Screen,self).__init__()
+
+        self.chat = self.ids.chat
+        print(self.chat)
+        for n in range(7):
+            if n%2:
+                aling = "left"
+            else: 
+                aling = "right"
+            label = MDLabel(
+                            text = str(n),
+                            font_size="5",
+                            halign = "right",
+                            valight="top",
+                            markup=True
+            )
+            self.chat.add_widget(label)
+
+
+
+class MainApp(MDApp):
+    def build(self):
+        screen = Screen()
+
+        return screen
+
+
+if __name__=="__main__":
+    MainApp().run()
